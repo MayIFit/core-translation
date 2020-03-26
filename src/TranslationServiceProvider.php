ce8dd7ad8199ff0793056124b7b616a92f559f25
Tischler Kristóf <tischler.kristof@gmail.com>
2020-03-26 18:14:46 +0100

    Add permission middleware to routes

--- src/TranslationServiceProvider.php
+++ src/TranslationServiceProvider.php
@@ -3,6 +3,9 @@
 
     use Illuminate\Support\ServiceProvider;
 
+    use MayIFit\Core\Permission\Models\Permission; 
+    use MayIFit\Core\Permission\Policies\PermissionPolicy; 
+
     class TranslationServiceProvider extends ServiceProvider {
 
         /**
@@ -10,9 +13,13 @@
          *
          * @var array
          */
+        protected $policies = [
+            Translation::class => TranslationPolicy::class,
+        ];
 
         public function boot() {
             $this->loadMigrationsFrom(__DIR__.'/Database/migrations');
+            $this->loadRoutesFrom(__DIR__.'/routes/api.php');
         }
 
         public function register() {
--- src/routes/api.php
+++ src/routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::group(['middleware' => ['api', 'auth:api'], 'prefix' => 'api/v1'], function () {
+Route::group(['middleware' => ['api', 'auth:api', 'mayifit/core-permission'], 'prefix' => 'api/v1'], function () {
     Route::namespace('MayIFit\Core\Translation\Http\Controllers\API')->prefix('admin/translation')->name('api.admin.')->group(function() { 
         Route::apiResource('translation', 'TranslationController@', ['except' => ['create', 'edit', 'destroy']]);
     });
